61ffccae28b48e334d31c2c9d5bda2eabbf4fb72
Thomas Blein <tblein@tblein.eu>
2018-05-08 15:06:53 +0200

    In event as a copy of in group

---
+++ CRM/CivirulesConditions/Contact/InEvent.mgd.php
@@ -0,0 +1,17 @@
+<?php
+
+return array (
+  0 =>
+    array (
+      'name' => 'Civirules:Condition.Contact.InEvent',
+      'entity' => 'CiviRuleCondition',
+      'params' =>
+        array (
+          'version' => 3,
+          'name' => 'contact_in_event',
+          'label' => 'Contact (not) in event',
+          'class_name' => 'CRM_CivirulesConditions_Contact_InEvent',
+          'is_active' => 1
+        ),
+    ),
+);
